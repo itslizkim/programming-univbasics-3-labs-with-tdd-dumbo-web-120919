47f146cd10f9cd3be8c167ddb9a0471f9be9be90
Elizabeth Kim <elizabeth.kim93@gmail.com>
2019-11-02 03:18:16 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -5,18 +5,4 @@ difference = first_number - second_number
 product = first_number * second_number
 quotient = first_number / second_number
 
-def sum(first_number,second_number)
-  puts first_number + second_number
-end
-
-def difference(first_number,second_number)
-  puts first_number - second_number
-end
-
-def product(first_number,second_number)
-  puts first_number * second_number
-end
-
-def quotient(first_number,second_number)
-  puts first_number / second_number
-end
+sum(first_number,second_number)
